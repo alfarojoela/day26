a1eb411d109f949dbf4c5a9f80c80851a2c1e421
Joel Alfaro <alfarojoela@outlook.com>
2021-09-25 18:16:47 -0700

    additional practice

--- main.py
+++ main.py
@@ -12,3 +12,21 @@ for n in numbers:
     n = n+1
     new_list_other_way.append(n)
 print(new_list_other_way)
+
+double_list = [(index*2) for index in range(1,5)]
+print(double_list)
+
+names = ['Alex', 'Beth', 'Caroline', 'Dave', 'Eleanor', 'Freddie']
+short_names = [name_item.upper() for name_item in names if len(name_item) >= 5]
+print(short_names)
+
+numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+# 🚨 Do Not Change the code above 👆
+
+#Write your 1 line code 👇 below:
+
+squared_numbers = [number * number for number in numbers]
+
+#Write your code 👆 above:
+
+print(squared_numbers)
